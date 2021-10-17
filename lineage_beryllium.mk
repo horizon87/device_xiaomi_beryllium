0fd83bc2a11bc7bce67ab4fb8f91d36608e17f26
NEXUS 003 <shuklakantmani123@gmail.com>
2021-10-17 03:22:16 +0000

    Updated Fingerprint

--- lineage_beryllium.mk
+++ lineage_beryllium.mk
@@ -17,11 +17,12 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SPB5.210812.002/7671067:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys" \
-    PRODUCT_NAME="beryllium"
+    PRIVATE_BUILD_DESC="redfin-user 12 SPB5.210812.002 7671067 release-keys" \
+    PRODUCT_NAME="beryllium" \
+    TARGET_DEVICE="beryllium" \
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
